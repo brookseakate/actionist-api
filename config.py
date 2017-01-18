7f0631b2b064c78a147e746cf33a2f906e734f6f
Kate Shaffer <kate.shaffer@gmail.com>
2017-01-18 15:43:37 -0800

    Added .env handling & unignored config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# add SQLAchemy URI for Postgres.app localhost
+SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+HTTP_AUTH_PASSWORD = os.environ['HTTP_AUTH_PASSWORD']
